80f00359181938dc2952eba718b48b1b14a75022
RLH55 <2.68785198e+08+RLH55@users.noreply.github.com>
2026-07-14 22:40:12 +0000

    Final Clean Deployment for Replit

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.flask
+    pkgs.python311Packages.werkzeug
+    pkgs.python311Packages.psutil
+  ];
+}
